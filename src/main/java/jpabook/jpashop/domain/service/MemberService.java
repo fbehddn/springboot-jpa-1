f3bbcf6b69166390acd6eec6d16162d57dfbe351
fbehddn <tom0611@naver.com>
2023-09-28 23:39:16 +0900

    회원 서비스 주석 제거 및 가시화

--- src/main/java/jpabook/jpashop/domain/service/MemberService.java
+++ src/main/java/jpabook/jpashop/domain/service/MemberService.java
@@ -11,15 +11,9 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor //private 필드에 대해서만 생성자 자동생성 및 생성자주입 선언
+@RequiredArgsConstructor
 public class MemberService {
-
     private final MemberRepository memberRepository;
-
-//    @Autowired
-//    public MemberService(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
 
     /**
      * 회원 가입
@@ -38,7 +32,7 @@ public class MemberService {
      */
     private void validateDuplicateMember(Member member) {
         //EXCEPTION
-        List<Member> findMembers = memberRepository.findByName(member.getUsername());
+        List<Member> findMembers = memberRepository.findByName(member.getName());
         if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
