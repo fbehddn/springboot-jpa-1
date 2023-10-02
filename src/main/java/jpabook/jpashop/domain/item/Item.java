61eddd74826ddeab1e012843a67440b65d418947
fbehddn <tom0611@naver.com>
2023-10-03 00:03:40 +0900

    fix: addStock 메서드 로직 수정

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -35,14 +35,14 @@ public abstract class Item {
      * stock 증가
      */
     public void addStock(int quantity) {
-        this.stockQuantity += stockQuantity;
+        this.stockQuantity += quantity;
     }
 
     /**
      * stock 감소
      */
     public void removeStock(int quantity) {
-        int restStock = stockQuantity - quantity;
+        int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
         }
